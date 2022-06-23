28d512231c832e9e73896ea663d464a9124cf927
armands <armandjermaks@gmail.com>
2022-06-23 17:40:30 +0300

    migrations/tables are added

---
+++ database/migrations/2022_06_22_142806_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->foreignId('employee_ID')->references('id')->on('users');
+            $table->string('description');
+            $table->id();
+            $table->string('term');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
