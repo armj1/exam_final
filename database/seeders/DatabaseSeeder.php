623821b3bb95eb853a8b6281ca4eb4db1a36890f
armands <armandjermaks@gmail.com>
2022-06-25 14:24:02 +0300

    three new users seeded into users table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +23,35 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        DB::table('users')->insert([
+            'id' => '1',
+            'email' => 'admin@app.com',
+            'name' => 'admin',
+            'surname' => 'admin',
+            'department' => 'admin',
+            'role' => '1',
+            'password' => bcrypt('admin123'),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => '2',
+            'email' => 'annalu@app.com',
+            'name' => 'Anna',
+            'surname' => 'Lu',
+            'department' => 'Finance department',
+            'role' => '2',
+            'password' => bcrypt('parole123'),
+        ]);
+
+        DB::table('users')->insert([
+            'id' => '3',
+            'email' => 'nori@app.com',
+            'name' => 'Nelly',
+            'surname' => 'Adams',
+            'department' => 'Marketing department',
+            'role' => '2',
+            'password' => bcrypt('parole456'),
+        ]);
     }
 }
